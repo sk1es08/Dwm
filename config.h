54e53aa5a96413159cb86b1c3f07ab3049dfc405
sk1es08 <akashvishwa780879@gmail.com>
2024-06-02 06:47:33 +0530

    fix dwm status colors

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont};
 static const char *termcmd[]  = { "st", NULL };
 #include "movestack.c"
 static const Key keys[] = {
